3bfc52a3b6b4d6ca019dad952037df2934623e59
LevDimka <Lev2000.dima@gmail.com>
2022-06-03 16:51:59 +0300

    Исправил ошибку

--- WEB_UI/Models/UserRegister.cs
+++ WEB_UI/Models/UserRegister.cs
@@ -7,12 +7,12 @@ namespace WEB_UI.Models
     using System.Data.Entity.Spatial;
 
     [Table("Pacient")]
-    public class UserRegister: Pacient
+    public class UserRegister //: Pacient
     {
-        [Required]
-        [Display(Name = "Подтвердите Пароль")]
-        [StringLength(50)]
-        [DataType(DataType.Password)]
-        public string ConfirmPassword { get; set; }
+        //[Required]
+        //[Display(Name = "Подтвердите Пароль")]
+        //[StringLength(50)]
+        //[DataType(DataType.Password)]
+        //public string ConfirmPassword { get; set; }
     }
 }
